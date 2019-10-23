4be0e788e0c1a85354a79d7088ed12113d06aacc
Tara Matthew <taramatthew.93@gmail.com>
2019-10-23 12:34:25 +0100

    Set tokens when user registers

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -62,6 +62,8 @@
                         email: this.email,
                         password: this.password
                     })
+                    this.$store.dispatch('setToken', response.data.token)
+                    this.$store.dispatch('setUser', response.data.user)
                     console.log(response.data)
                 } catch(error) {
                     this.error = error.response.data.error
