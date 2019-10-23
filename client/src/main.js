d38c1be30a32b9d7c17c06ac068456c0ded81e52
Tara Matthew <taramatthew.93@gmail.com>
2019-10-23 12:33:54 +0100

    Using vuex store

--- client/src/main.js
+++ client/src/main.js
@@ -3,10 +3,16 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
 
+import store from './store/store'
+import {sync} from 'vuex-router-sync'
+
 Vue.config.productionTip = false
+
+sync(store, router)
 
 new Vue({
   render: h => h(App),
   vuetify,
-  router
+  router,
+  store
 }).$mount('#app')
