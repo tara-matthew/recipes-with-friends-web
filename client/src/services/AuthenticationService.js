f474a96ed94c8ea7ad604f623e4ffbec2404ce9c
Tara Matthew <taramatthew.93@gmail.com>
2019-10-23 10:54:40 +0100

    Login function and server route

--- client/src/services/AuthenticationService.js
+++ client/src/services/AuthenticationService.js
@@ -4,4 +4,8 @@ export default {
     register (credentials) {
         return Api().post('register', credentials)
     },
+    
+    login(credentials) {
+        return Api().post('login', credentials)
+    }
 }
