7387c6f37ae3ae2757c4a3814995f85a5e11e7c3
Tara Matthew <taramatthew.93@gmail.com>
2019-11-20 12:41:30 +0000

    Changed View Recipe page to use flexbox

--- client/src/components/ViewRecipe.vue
+++ client/src/components/ViewRecipe.vue
@@ -1,32 +1,84 @@
 <template>
-
-    <div class="wrapper">
+    <div>
         <page-header />
 
-        <div class="box title-box">
-            <div class="photo-box">
-                <p>Photo here</p>
-            </div>
-            <p>Recipe title goes here</p>
-        </div>
+        <v-container fill-height fluid>
+            <v-row>
+                <v-col
+                    cols="12"
+                    class="custom-padding pt-7">
 
-        <div class="box ingredients-box">
-            <div class="ingredients-inner-div">
-                <h1 class=centred>Ingredients</h1>
-                <ul>
-                    <li>
-                        Sexy tasty Caroline
-                    </li>
-                </ul>
-            </div>
-        </div>
+                    <panel
+                        title="View Recipe"
+                        panelColour="#099E7A"
+                        class="title-box">
 
-        <div class="box tips-box">
-            <div class="tips-inner-div">
-                <h1 class=centred>Hot Tips</h1>
-            </div>
-        </div>
+                        <v-row>
+                            <v-col
+                                align-self="center"
+                                class="px-12 py-12"
+                                cols="5">
+                                <div class="photo-box">
+                                    <p class="photo-text">Photo here</p>
+                                </div>
+                            </v-col>
+
+                            <v-col
+                                cols="7"
+                                class="px-12 py-12">
+
+                                <div class="info-box">
+                                    <h2>Sexy ramen</h2>
+
+                                    <div class="details">
+                                        <v-row>
+                                            <v-col
+                                                cols="6"
+                                                class="pb-0">
+                                                <p class="bold-text">Prep:</p>
+                                                <p class="bold-text">Cook:</p>
+
+                                            </v-col>
+
+                                            <v-col
+                                                cols="6"
+                                                align-self="center">
+                                                <p class="no-margin bold-text">Eaten:</p>
+                                            </v-col>
+                                        </v-row>
+                                    </div>
+                                </div>
+
+                            </v-col>
+                        </v-row>
+                    </panel>
+                </v-col>
+            </v-row>
+
+            <v-row>
+                <v-col
+                    cols="4"
+                    class="custom-padding pt-7">
+                    <v-card
+                        justify-items="center">
+                        <v-card-title class="card-title">
+                            Ingredients
+                        </v-card-title>
+                        <div class="inner-container">
+                            <p>Test</p>
+                            <p>Test</p>
+                            <p>Test</p>
+                            <p>Test</p>
+                        </div>
+
+
+                    </v-card>
+                </v-col>
+            </v-row>
+
+        </v-container>
     </div>
+
 
 </template>
 
@@ -43,6 +95,11 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
+    .custom-padding {
+        padding-left: 39px;
+        padding-right: 39px;
+    }
+
     .panel-title {
         text-align: center;
         width: 100%;
@@ -51,52 +108,50 @@ export default {
         font-size: 24px;
     }
 
-    .wrapper {
-        display: grid;
-        grid-template-columns: repeat(12, [col-start] 1fr);
-        grid-template-rows: repeat(14, 1fr);
-        grid-column-gap: 10px;
-        grid-row-gap: 40px;
-    }
-
-    .box {
-        border: 1px solid black;
-    }
-
-    .title-box {
-        grid-column: 2/12;
-        grid-row: 2/5;
-    }
-
-    .ingredients-box {
-        grid-column: 2/6;
-        grid-row: 5/11;
-    }
-
-    .tips-box {
-        grid-column: 7/12;
-        grid-row: 5/11;
-        padding: 1px;
-    }
-
-    .ingredients-inner-div {
-        /* border: 2px solid black; */
-        height: 100%;
-    }
-
-    .tips-inner-div {
-        height: 100%;
-        margin-left: auto;
-        margin-right: 0;
-    }
-
-    .centred {
-        text-align: center;
-    }
-
     .photo-box {
         border: 1px solid black;
-        width: 30%;
+        height: 259px;
+        position: relative;
+    }
+
+    .photo-text {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-weight: bold;
+    }
+
+    .info-box {
+        border: 1px solid black;
+        height: 259px;
+        position: relative;
+    }
+
+    .details {
+        border: 1px solid black;
+        margin-top: 20px;
+        position: absolute;
+        /* height: 84px; */
+        bottom: 0;
+        width: 100%;
+    }
+
+    p.no-margin {
+        margin-bottom: 0;
+    }
+
+    .bold-text {
+        font-weight: bold;
+    }
+
+    .card-title {
+        justify-content: center;
+    }
+
+    .inner-container {
+        min-height: 300px;
+        margin-left: 39px;
     }
 
 </style>
