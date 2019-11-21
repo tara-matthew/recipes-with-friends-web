16a4be3cec2e9601fa6c86bd825a17c26876d2a8
Tara Matthew <taramatthew.93@gmail.com>
2019-11-21 15:52:08 +0000

    Upload route to api

--- client/src/services/AuthenticationService.js
+++ client/src/services/AuthenticationService.js
@@ -4,8 +4,12 @@ export default {
     register (credentials) {
         return Api().post('register', credentials)
     },
-    
+
     login(credentials) {
         return Api().post('login', credentials)
+    },
+
+    upload(file) {
+        return Api().post('upload', file)
     }
 }
