2e6d756adef15be7f7f85305aca07043647c8581
Tara Matthew <taramatthew.93@gmail.com>
2020-01-08 10:34:23 +0000

    Set up seed data for recipes

--- server/seed/index.js
+++ server/seed/index.js
@@ -1,17 +1,25 @@
 const {
     sequelize,
-    User
+    User,
+    Recipe
 } = require('../src/models')
 
 // Bluebird is to make sure that all the database has been seeded before doing anything else
 const Promise = require('bluebird')
 const users = require('./users.json')
+const recipes = require('./recipes.json')
 
 sequelize.sync({force:true})
     .then(async function() {
         await Promise.all(
             users.map(user => {
                 User.create(user)
+            })
+        )
+
+        await Promise.all(
+            recipes.map(recipe => {
+                Recipe.create(recipe)
             })
         )
     })
